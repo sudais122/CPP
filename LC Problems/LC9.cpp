eb4d4fb5dd8a80593f45dff13fd91e18b8f5c8b2
sudais122 <khansb17798@gmail.com>
2025-09-10 00:24:10 +0500

    Account Updated

---
+++ LC Problems/LC9.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+void rotate()
+{
+    int nums[] = {1,2,3,4,5,6,7};
+
+    int n =6;
+
+    for (int i = n; i >= 0; i--)
+    {
+        cout << nums[i] << " ";
+    }
+
+    cout << "Second rotation" <<endl;
+    for (int i = 2; i >= 0; i--)
+    {
+        cout << nums[i] <<" ";
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << nums[i] <<" ";
+    }
+    
+    
+    
+
+//     Rotate by 1 → [7,1,2,3,4,5,6]
+
+// Rotate by 2 → [6,7,1,2,3,4,5]
+
+// Rotate by 3 → [5,6,7,1,2,3,4]
+    
+}
+int main() {
+    rotate();
+}
--- LC Problems/tempCodeRunnerFile.cpp
+++ LC Problems/tempCodeRunnerFile.cpp
@@ -1,47 +1,4 @@
-#include<iostream>
-using namespace std;
-
-void majoreleme()
-{
-   int arr[] = {2, 2, 1, 3, 2, 1}, n = 6;   
-
-   for (int i = 0; i < n; i++) // loop through all th element of the array
-   {
-        int count = 1;
-        bool visited = false;
-        int majorelem;
-
-        for (int j = 0; j < i; j++)
+        for (int i = 0; i < n; i++)
         {
-            if (arr[i] == arr[j]) // if the element is already visited or count it skip it 
-            {
-                visited = true;
-                break;
-            }
+            cout << nums[i] <<" ";
         }
-
-        if(visited) continue;  // if the element is already count then skip it 
-
-        for (int k = i+1; k < n; k++) // it compare the urrent element to all the lement if the.
-        {
-            if (arr[i] == arr[k])
-            {
-                count++;
-            }
-            
-        }
-
-        if (count > n/2)
-        {
-            majorelem = arr[i];
-            cout << "Major Element is" << majorelem <<endl;
-            break;
-
-        }
-   }
-   
-}
-int main() {
-    majoreleme();
-    return 0;
-}
